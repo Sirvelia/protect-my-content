870fac09232d7e74dcb0ed994cc0cdce0dcfdd30
Amadeu Arderiu <amadeu@sirvelia.com>
2023-10-09 16:13:06 +0200

    disable text selection

---
+++ Protections/DisableTextSelect.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ProtectMyContent\Protections;
+
+class DisableTextSelect
+{
+
+    protected $plugin_name;
+    protected $plugin_version;
+
+    public function __construct($plugin_name, $plugin_version)
+    {
+        $this->plugin_name = $plugin_name;
+        $this->plugin_version = $plugin_version;
+
+        add_action('wp_head', [$this, 'render_script'], 90);
+    }
+
+    public function render_script()
+    {
+        echo "<style>body {
+        -webkit-touch-callout: none; /* iOS Safari */
+            -webkit-user-select: none; /* Safari */
+            -khtml-user-select: none; /* Konqueror HTML */
+            -moz-user-select: none; /* Old versions of Firefox */
+                -ms-user-select: none; /* Internet Explorer/Edge */
+                    user-select: none; /* Non-prefixed version, currently
+                                        supported by Chrome, Edge, Opera and Firefox */
+        }</style>";
+    }
+}
--- data/settings.php
+++ data/settings.php
@@ -8,5 +8,11 @@ return [
         'name' => __('Disable Contextual Menu', 'protect-my-content'),
         'type' => 'checkbox',
         'description' => __('Check this to disable acess to contextual menu', 'protect-my-content'),
-    ]
+    ],
+    [
+        'slug' => 'disable-text-select',
+        'name' => __('Disable Text Selection', 'protect-my-content'),
+        'type' => 'checkbox',
+        'description' => __('Check this to disable text selection', 'protect-my-content'),
+    ],
 ];
